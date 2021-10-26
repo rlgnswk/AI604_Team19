32ec5d4801b19001f03fc62fbf29dcf4453d6204
yujin05 <92221241+yujin05@users.noreply.github.com>
2021-10-26 15:24:01 +0900

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -1,36 +1,68 @@
 import torch
-import torch.nn as nn #
-from torch.utils import data
-import torchvision
 import torch.nn.functional as F
 import random
 import os
 import numpy as np
 import cv2
 import os.path
+from torch.autograd import Variable
+from PIL import Image
 
-
-def dataAug(imglr):
+def dataAug(imglr, imghr):
     if random.random() < 0.3:  # horizontal flip
         imglr = imglr[:, ::-1, :]
-
-
+        imghr = imghr[:, ::-1, :]
     if random.random() < 0.3:  # vertical flip
-        imglr =  imglr[::-1, :, :]
+        imglr = imglr[::-1, :, :]
+        imghr = imghr[:, ::-1, :]
 
     rot = random.randint(0, 3)  # rotate
     imglr = np.rot90(imglr, rot, (0, 1))
+    imghr = np.rot90(imghr, rot, (0, 1))
 
-    crop_size_w=random.randint(0,40)
-    crop_size_h=random.randint(0,40)
+    # crop_size_w=random.randint((30,300))
+    # crop_size_h=random.randint(30,300)
+    crop_size_w = 128
+    crop_size_h = 128
+    crop_size_w1 = 256
+    crop_size_h1 = 256
 
-    input_size_h, input_size_w, _ = imglr[0].shape
-    x_start = random.randint(0, input_size_w - crop_size_w)
-    y_start = random.randint(0, input_size_h - crop_size_h)
+    input_size_h, input_size_w, _ = imglr.shape
 
-    img_lr = imglr[y_start: y_start + crop_size_h, x_start: x_start + crop_size_w]
 
-    return  img_lr
+
+
+    x_start=random.randrange(0, input_size_w-crop_size_w)
+    y_start = random.randint(0, input_size_h -crop_size_h)
+    (x_gt, y_gt)=(2*x_start, 2*y_start)
+    imglr = imglr[y_start: y_start+crop_size_h,x_start : x_start + crop_size_w , :]
+    imghr = imghr[y_gt: y_gt+crop_size_h1,x_gt : x_gt + crop_size_w1 , :]
+
+
+    return imglr, imghr
+
+
+def get_aug(data_dir):
+    data_lr=os.path.join(data_dir, "LR")
+    data_hr=os.path.join(data_dir, "ILR")
+    fileList1 = os.listdir(data_lr)
+    fileList2 = os.listdir(data_hr)
+    nTrain = len(fileList1)
+    train_images=[]
+    GT_images=[]
+    for i in range(1):
+        name = fileList1[1]
+        nameTrain = os.path.join(data_lr, name)
+        nameGT = os.path.join(data_hr, name)
+        imgTrain = cv2.imread(nameTrain)
+        imgGT=cv2.imread(nameGT)
+        for j in range(100):
+            train_aug, GT_aug = dataAug(imgTrain, imgGT)
+            train_images.append(train_aug)
+            GT_images.append(GT_aug)
+
+    return train_images, GT_images
+
 
 def RGB_np2Tensor(img_lr):
 
@@ -47,25 +79,24 @@ class Dataset(torch.utils.data.dataset.Dataset):
         self.args=args
         self.dirpath = args.datasetPath
         self.datatype=args.datasettype
-        self.final_path=os.path.join(self.dirpath, self.datatype)
-        self.filelist = os.listdir(self.final_path)
-        self.img_path = os.path.join(self.final_path, 'Test')
-        self.filefin = os.listdir(self.img_path)
-
+        self.kerneltype=args.kerneltype
+        self.data_dir = os.path.join(self.dirpath, self.datatype, self.kerneltype)  # /datasets/Set5/g02
+        train_images, GT_images = get_aug(self.data_dir)
+        self.tr_im=train_images
+        self.gt_im=GT_images
 
     def __len__(self):
 
-        length=len(self.filefin)
+        length=len(self.tr_im)
         return length
 
     def __getitem__(self, idx):
         args = self.args
-        imgIn1 = cv2.imread(self.img_path)
-        imgIn1=RGB_np2Tensor(imgIn1)
-        imgIn1 = downscale(imgIn1)
+        img_lr = self.tr_im[idx]
+        img_gt=self.gt_im[idx]
 
-        return imgIn1
+        img_lr=RGB_np2Tensor(img_lr)
+        img_gt = RGB_np2Tensor(img_gt)
 
-def downscale(img_aug):
-    img_aug=F.interpolate(input=img_aug, scale_factor=0.5, mode='bicubic')
-    return img_aug
+        return img_lr, img_gt
+
