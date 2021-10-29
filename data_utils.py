57bcef28675bc1bcef20d1a195689379b9450789
yujin05 <92221241+yujin05@users.noreply.github.com>
2021-10-29 21:41:03 +0900

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -7,39 +7,45 @@ import cv2
 import os.path
 from torch.autograd import Variable
 from PIL import Image
+import torch.nn as nn
+
+
 
 def dataAug(imglr, imghr):
-    if random.random() < 0.3:  # horizontal flip
-        imglr = imglr[:, ::-1, :]
-        imghr = imghr[:, ::-1, :]
-    if random.random() < 0.3:  # vertical flip
-        imglr = imglr[::-1, :, :]
-        imghr = imghr[:, ::-1, :]
-
-    rot = random.randint(0, 3)  # rotate
-    imglr = np.rot90(imglr, rot, (0, 1))
-    imghr = np.rot90(imghr, rot, (0, 1))
-
-    # crop_size_w=random.randint((30,300))
-    # crop_size_h=random.randint(30,300)
-    crop_size_w = 128
-    crop_size_h = 128
-    crop_size_w1 = 256
-    crop_size_h1 = 256
-
-    input_size_h, input_size_w, _ = imglr.shape
+    imglr=RGB_np2Tensor(imglr)
+    imghr=RGB_np2Tensor(imghr)
+    a = imglr.shape[1]
+    b = imglr.shape[2]
+    c = min(a, b)
 
 
+    train_lr=[]
+    train_hr=[]
 
+    for i in range(10):
+        k = random.randint(130, c)
+        img_lr = torch.unsqueeze(imglr, 0)
+        img_hr = torch.unsqueeze(imghr, 0)
+        img_lr=F.interpolate(img_lr, scale_factor=k/c, mode='bicubic')
+        img_hr = F.interpolate(img_hr, scale_factor=k/c, mode='bicubic')
+        img_lr = torch.squeeze(img_lr, dim=0)
+        img_hr = torch.squeeze(img_hr, dim=0)
+        train_lr.append(img_lr)
+        train_hr.append(img_hr)
+        for j in range(4):
+            img_lr= torch.rot90(img_lr, j, (1, 2))
+            img_hr = torch.rot90(img_hr, j, (1,2))
+            for k in range(2):
+                if k==0:
+                    img_lr = img_lr.flip(1)
+                    img_hr = img_hr.flip(1)
+                if k==1:
+                     img_lr = img_lr.flip(2)
+                     img_hr = img_hr.flip(2)
+                train_lr.append(img_lr)
+                train_hr.append(img_hr)
+    return train_lr, train_hr
 
-    x_start=random.randrange(0, input_size_w-crop_size_w)
-    y_start = random.randint(0, input_size_h -crop_size_h)
-    (x_gt, y_gt)=(2*x_start, 2*y_start)
-    imglr = imglr[y_start: y_start+crop_size_h,x_start : x_start + crop_size_w , :]
-    imghr = imghr[y_gt: y_gt+crop_size_h1,x_gt : x_gt + crop_size_w1 , :]
-
-
-    return imglr, imghr
 
 
 def get_aug(data_dir):
@@ -48,20 +54,17 @@ def get_aug(data_dir):
     fileList1 = os.listdir(data_lr)
     fileList2 = os.listdir(data_hr)
     nTrain = len(fileList1)
-    train_images=[]
-    GT_images=[]
+
     for i in range(1):
         name = fileList1[1]
         nameTrain = os.path.join(data_lr, name)
         nameGT = os.path.join(data_hr, name)
         imgTrain = cv2.imread(nameTrain)
         imgGT=cv2.imread(nameGT)
-        for j in range(100):
-            train_aug, GT_aug = dataAug(imgTrain, imgGT)
-            train_images.append(train_aug)
-            GT_images.append(GT_aug)
+        train_aug, GT_aug = dataAug(imgTrain, imgGT)
 
-    return train_images, GT_images
+
+    return train_aug, GT_aug
 
 
 def RGB_np2Tensor(img_lr):
@@ -94,9 +97,18 @@ class Dataset(torch.utils.data.dataset.Dataset):
         args = self.args
         img_lr = self.tr_im[idx]
         img_gt=self.gt_im[idx]
+        crop_size_w = 128
+        crop_size_h = 128
+        crop_size_w1 = 256
+        crop_size_h1 = 256
+        input_size_h = img_lr.shape[1]
+        input_size_w = img_lr.shape[2]
 
-        img_lr=RGB_np2Tensor(img_lr)
-        img_gt = RGB_np2Tensor(img_gt)
+        x_start = random.randrange(0, input_size_w - crop_size_w)
+        y_start = random.randint(0, input_size_h - crop_size_h)
+        (x_gt, y_gt) = (2 * x_start, 2 * y_start)
+        img_lr = img_lr[:, y_start: y_start + crop_size_h, x_start: x_start + crop_size_w]
+        img_gt = img_gt[:, y_gt: y_gt + crop_size_h1, x_gt: x_gt + crop_size_w1]
 
         return img_lr, img_gt
 
@@ -125,3 +137,4 @@ class testDataset(torch.utils.data.dataset.Dataset):
 
 
         return img_hr
+
